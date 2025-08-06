af7e8e06597f989135359068b53a199a43952739
kbs0621 <jjjustin7531@gmail.com>
2025-08-06 17:21:25 +0900

    printTitle update

---
+++ src/workshop/person/control/PersonManager.java
@@ -0,0 +1,13 @@
+package workshop.person.control;
+
+public class PersonManager {
+
+	public static void main(String[] args) {
+		
+
+	}
+	
+	public void printTitle(String title) {
+		System.out.println("@@@" + title + "@@@");
+	}
+}
