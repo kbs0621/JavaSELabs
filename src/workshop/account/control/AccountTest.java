4ce240b2f8b2a001bff6c7c698a849de4c25eed1
kbs0621 <jjjustin7531@gmail.com>
2025-08-06 14:22:51 +0900

    기본 생성자

---
+++ src/workshop/account/control/AccountTest.java
@@ -0,0 +1,19 @@
+package workshop.account.control;
+
+import workshop.account.entity.Account;
+
+public class AccountTest {
+
+	public static void main(String[] args) {
+		// Account °´Ã¼ »ý¼º
+		Account account = new Account();
+		
+		//°í°´¹øÈ£ : "A1100", °èÁÂ¹øÈ£ : "221-22-3477", ÀÜ¾× : 100
+		account.setCustId("A1100");
+		account.setAcctId("221-22-3477");
+		account.setBalance(100);
+		
+		
+	}
+
+}
